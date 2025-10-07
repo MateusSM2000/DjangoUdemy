c62407f9562ab6833cd39c06ad757087b5c483ca
MateusSM2000 <matt_petao@hotmail.com>
2025-10-07 15:16:36 -0300

    update

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+import requests
 
 
 # Create your views here.
 
 def my_view(request):
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    if response.status_code == 200:
+        posts = response.json()
+    else:
+        posts = [{'Erro ao buscar os dados': response.status_code}]
+
     context = {
         'text': 'BLOG',
         'title': '- Blog',
+        'posts': posts,
     }
 
     return render(request, 'blog/index.html', context)
