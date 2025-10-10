8d692abf3a4f3723c4de75423f4862142e85ac61
MateusSM2000 <matt_petao@hotmail.com>
2025-10-09 21:08:23 -0300

    update

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', blog_views.my_view, name='index'),
     path('articles/', blog_views.articles_view, name='articles'),
-    path('post/<int:id>/', blog_views.post_view, name='post'),
+    path('<int:id>/', blog_views.post_view, name='post'),
 ]
